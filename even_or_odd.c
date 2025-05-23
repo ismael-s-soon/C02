348b8ed8219012ccf859fa509a48732187d8d121
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-23 02:03:14 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard 
 
 int main() {
-int nombre = 0;
-scanf("%d", &nombre);
-if ( nombre % 2 == 0 ) {
-printf ("%d est pair.\n", nombre);
-} else {
-printf ("%d est inpair.\n", nombre);
-}
-return 0;
+    int nombre = 0; // Déclaration et initialisation de la variable "nombre" à 0
+
+    scanf("%d", &nombre); // Lecture d'un entier saisi et stocker dans "nombre"
+
+    // Vérification si le nombre est pair
+    if ( nombre % 2 == 0 ) {
+        printf ("%d est pair.\n", nombre); // Affichage si le nombre est pair
+    } else {
+        printf ("%d est impair.\n", nombre); // Affichage si le nombre est impair
+    }
+
+    return 0; // Fin du programme
 }
