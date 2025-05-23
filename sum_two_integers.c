42dffb1d78622bc69ed26bc18fcafeb57f8a6f1a
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-23 01:58:48 +0100

    Update sum_two_integers.c

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard 
+
 int main() {
-int a = 0, b = 0;
-int resultat = 0;
-scanf("%d %d", &a, &b);
-resultat = a + b;
-printf("La somme est : %d\n", resultat);
-return 0;
+    int a = 0, b = 0; // Déclaration et initialisation de deux variables entières à 0
+    int resultat = 0; // Déclaration et initialisation de la variable resultat à 0
+
+    scanf("%d %d", &a, &b); // Lire les deux entiers saisis et stock dans a et b
+
+    resultat = a + b; // Calcul de la somme de a + b et stock dans resultat
+
+    printf("La somme est : %d\n", resultat); // Affichage du résultat
+  
+    return 0; // Fin du programme avec retour 0 
 }
