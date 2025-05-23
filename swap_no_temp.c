de17100ed4ca2cc49e547c65504e74f9f8211917
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-23 02:09:54 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard 
 
 int main() {
-int a, b;
-scanf("%d %d", &a, &b);
+    int a, b; // Déclaration de deux variables entières a et b
 
-a = a + b;
-b = a - b;
-a = a - b;
-printf("Après échange : a = %d, b = %d\n", a, b);
-return 0;
+    scanf("%d %d", &a, &b); // Lecture de deux entiers saisis par l'utilisateur
+
+    // Échange des valeurs de a et b sans utiliser de variable temporaire
+    a = a + b; // a contient maintenant la somme des deux nombres
+    b = a - b; // b récupère l'ancienne valeur de a
+    a = a - b; // a récupère l'ancienne valeur de b
+
+    // Affichage des valeurs après l'échange
+    printf("Après échange : a = %d, b = %d\n", a, b);
+
+    return 0; // Fin du programme
 }
