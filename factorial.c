4a1105f61973ac5d38eb562067c53bed7740a77f
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-23 02:07:25 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,18 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard 
 
 int main() {
-int nombre = 0;
-int i = 1;
-scanf("%d", &nombre);
-if (nombre < 0) {
-printf ("Erreur.\n");
-} else {
-int fact = 1; 
-while (i <= nombre) {
-fact *= i;
-i++;
-}
-printf("Le factoriel de %d est : %d\n", nombre, fact);
-}
-return 0;
+    int nombre = 0; // Déclaration et initialisation de la variable "nombre" à 0
+    int i = 1;       // Déclaration et initialisation du compteur "i" à 1
+
+    scanf("%d", &nombre); // Lecture d'un entier saisi et stocker dans "nombre"
+
+    // Vérifie si le nombre est négatif
+    if (nombre < 0) {
+        printf("Erreur.\n"); // Affiche un message d'erreur si le nombre est négatif
+    } else {
+        int fact = 1; // Déclaration et initialisation de la variable "fact" à 1
+
+        // Boucle pour calculer le factoriel de "nombre"
+        while (i <= nombre) {
+            fact *= i; // Multiplie "fact" par "i"
+            i++;       // Incrémente "i"
+        }
+
+        // Affiche le résultat du factoriel
+        printf("Le factoriel de %d est : %d\n", nombre, fact);
+    }
+
+    return 0; // Fin du programme
 }
